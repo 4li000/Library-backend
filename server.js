7582ec4aa51fa54f4494b1baf6b0072c9d9b502c
Ali Albanna <ali@albanna.com>
2024-04-02 00:24:05 +0300

    add db config, dotenv npm

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 
 const app = express();
+require('dotenv').config();
+require("./config/database")
 
 app.listen(3000, () => {
     console.log("Listening on port 3000");
