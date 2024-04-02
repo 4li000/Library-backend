a7f293e2ee9e98a2b7386f4e30d8e7c11e3cefbf
Ali Albanna <ali@albanna.com>
2024-04-02 21:13:27 +0300

    seed file has a good structure for the books info

---
+++ seed.js
@@ -0,0 +1,31 @@
+const axios = require("axios");
+
+const queryApi = async () => {
+    let response = await axios.get("https://www.googleapis.com/books/v1/volumes?q=atomic+habits&langRestrict=en");
+    // console.log(response.data)
+
+    response.data.items.forEach(book => {
+        if( book.volumeInfo.title &&
+            book.volumeInfo.authors &&
+            book.volumeInfo.publisher &&
+            book.volumeInfo.publishedDate &&
+            (book.volumeInfo.industryIdentifiers && book.volumeInfo.industryIdentifiers[0].type.includes("ISBN")) &&
+            (book.volumeInfo.imageLinks && book.volumeInfo.imageLinks.thumbnail) &&
+            book.volumeInfo.language === "en" &&
+            book.volumeInfo.description
+            )  
+            {
+                console.log(book.volumeInfo.title);
+                console.log(book.volumeInfo.authors);
+                console.log(book.volumeInfo.publisher);
+                console.log(book.volumeInfo.publishedDate);
+                console.log(book.volumeInfo.description);
+                console.log(book.volumeInfo.industryIdentifiers[0]);
+                console.log(book.volumeInfo.imageLinks && book.volumeInfo.imageLinks.thumbnail);
+                console.log("======================================");
+            }
+    });
+
+}
+
+queryApi();
--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 
+
 const app = express();
 require('dotenv').config();
 require("./config/database")
+
 
 app.listen(3000, () => {
     console.log("Listening on port 3000");
